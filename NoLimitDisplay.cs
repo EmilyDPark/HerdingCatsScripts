ed1d97c11ef90e9cff21ef2f0cdf6d0fe53cf38d
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-03-31 14:09:04 -0700

    GUI for No Limit

---
+++ NoLimitDisplay.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoLimitDisplay : MonoBehaviour
+{
+    public Texture BoxTexture; // Drag a Texture onto this item in the Inspector
+
+    GUIContent content;
+    GUIStyle style = new GUIStyle();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        content = new GUIContent("No Limit\n - Unlimited Time\n - Unlimited Fences", BoxTexture, "This is a tooltip");
+
+        // Position the Text and Texture in the center of the box
+        style.alignment = TextAnchor.MiddleCenter;
+        // Position the Text below the Texture (rather than to the right of it)
+        style.imagePosition = ImagePosition.ImageAbove;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnGUI()
+    {
+        GUI.Box(new Rect(0, 0, Screen.width, Screen.height), content, style);
+    }
+}
