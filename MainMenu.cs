443f83838a14cafdd8e142da92b3c909b433b2f0
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-03-31 14:08:03 -0700

    Create MainMenu.cs

---
+++ MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string playOptionsScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(playOptionsScene);
+    }
+
+    public void Options()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
