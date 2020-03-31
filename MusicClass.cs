11df641e435742ec465f5e505e1a42dee60f56fe
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-03-31 14:08:30 -0700

    DontDestroyOnLoad to keep music playing through scene changes

---
+++ MusicClass.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicClass : MonoBehaviour
+{
+    static MusicClass running = null;
+
+    private AudioSource _audiosource;
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        _audiosource = GetComponent<AudioSource>();
+
+        if (running == null)
+        {
+            _audiosource.Play();
+            running = this;
+        }
+        else
+        {
+            if (_audiosource.isPlaying)
+                return;
+        }
+    }
+
+    public void PlayMusic()
+    {
+        if (_audiosource.isPlaying) return;
+        _audiosource.Play();
+    }
+    
+    public void StopMusic()
+    {
+        _audiosource.Stop();
+    }
+}
